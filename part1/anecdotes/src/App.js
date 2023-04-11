21ced65c4cc337003200dda5bc9c666c64d8739b
Sergio Dorado <darksang.exodar@gmail.com>
2023-04-11 15:17:57 +0200

    Exercise 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -4,6 +4,26 @@ const Button = (props) => (
   <button onClick={props.handleClick}>{props.label}</button>
 )
 
+const MostVotedAnecdote = (props) => {
+  //console.log(props.mostVoted)
+  //console.log(props.anecdotes)
+  //console.log(props.anecdotesPoints)
+  if (props.mostVoted === -1) {
+    return (
+      <div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h1>Anecdote Most Voted</h1>
+      <p>{props.anecdotes[props.mostVoted]}</p>
+      <p>has {props.anecdotesPoints[props.mostVoted]} votes</p>
+    </div>
+  )
+}
+
 const App = () => {
   const anecdotes = [
     'If it hurts, do it more often.',
@@ -22,9 +42,11 @@ const App = () => {
   const randomNumber = Math.floor(Math.random() * anecdotes.length)
   const [selected, setSelected] = useState(randomNumber)
 
+  const [mostVoted, setMostVoted] = useState(-1)
+
   const selectRandomAnecdote = () => {
     const randomNumber = Math.floor(Math.random() * anecdotes.length)
-    console.log(randomNumber)
+    console.log('Generated Number:', randomNumber)
     setSelected(randomNumber)
   }
 
@@ -32,15 +54,26 @@ const App = () => {
     const newPoints = [ ...anecdotesPoints ]
     newPoints[selected] += 1
     setAnecdotesPoints(newPoints)
-    console.log('Points', newPoints)
+    updateMostVoted(newPoints)
+  }
+
+  const updateMostVoted = (points) => {
+    const highestPoints = Math.max(...points)
+    console.log('Highest Points:', highestPoints)
+    const highestIndex = points.indexOf(highestPoints)
+    console.log('Highest Index:', highestIndex)
+    setMostVoted(highestIndex)
   }
 
   return (
     <div>
+      <h1>Anecdote of the Day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {anecdotesPoints[selected]} votes</p>
       <Button handleClick={voteCurrentAnecdote} label='Vote' />
       <Button handleClick={selectRandomAnecdote} label='Next Anecdote' />
+
+      <MostVotedAnecdote mostVoted={mostVoted} anecdotes={anecdotes} anecdotesPoints={anecdotesPoints} />
     </div>
   )
 }
